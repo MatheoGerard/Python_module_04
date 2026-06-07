fb74e337c4e3b29ddecd2db0ccf6aa53b6286b04
mgerard <mgerard@student.42belgium.be>
2026-06-07 09:34:14 +0200

    add ex0 and test fileto read

--- ex0/ft_ancient_text.py
+++ ex0/ft_ancient_text.py
@@ -1,16 +1,22 @@
-import io
 import sys
+import typing
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         print("=== Cyber Archives Recovery ===")
         try:
             print(f"Accessing file '{sys.argv[1]}'")
-            file_open = open(sys.argv[1])
+            file_open: typing.TextIO = open(sys.argv[1])
+            print("---")
+            print()
+            print(file_open.read())
+            print()
+            print("---")
+            file_open.close()
+            print(f"File '{file_open.name}' closed.")
         except FileNotFoundError as e:
-            print(f"Error opening file '{sys.argv[1]}': {e}")
+            print(f"\033[31mError opening file '{sys.argv[1]}': {e}\033[0m")
         except PermissionError as e:
-            print(f"Error opening file '{sys.argv[1]}': {e}")
-        io.read()
+            print(f"\033[31mError opening file '{sys.argv[1]}': {e}\033[0m")
     else:
-        print("Usage: ft_ancient_text.py <file>")
+        print("\033[31mUsage: ft_ancient_text.py <file>\033[0m")
