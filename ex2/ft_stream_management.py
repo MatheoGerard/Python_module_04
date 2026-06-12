a0f8700587f2254abd47ca4065ee338272fe126e
Matheo Gerard <mgerard@fu-r4-p7.s19.be>
2026-06-12 15:06:05 +0200

    add all exos and verification I THINK

--- ex2/ft_stream_management.py
+++ ex2/ft_stream_management.py
@@ -19,9 +19,11 @@ def convert_text(text: str) -> list[str]:
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         print("=== Cyber Archives Recovery ===")
+        file_open: typing.TextIO | None = None
+        file_to_save: typing.TextIO | None = None
         try:
             print(f"Accessing file '{sys.argv[1]}'")
-            file_open: typing.TextIO = open(sys.argv[1])
+            file_open = open(sys.argv[1])
             print("---")
             print()
             text_in_file: str = file_open.read()
@@ -46,17 +48,22 @@ if __name__ == "__main__":
                 print("Not saving data.")
             else:
                 print(f"Saving data to '{saved_name}'")
-                file_to_save: typing.TextIO = open(saved_name, "w")
+                file_to_save = open(saved_name, "w")
                 file_to_save.write(new_text)
                 file_to_save.close()
                 print(f"Data saved in file '{saved_name}'.")
-        except FileNotFoundError as e:
+        except KeyboardInterrupt:
+            sys.stderr.write(
+                "\033[31m\n[STDERR] Error: interrupt input\033[0m"
+            )
+        except Exception as e:
             sys.stderr.write(
                 f"\033[31m[STDERR] Error opening file '{sys.argv[1]}': {e}\033[0m"
             )
-        except PermissionError as e:
-            sys.stderr.write(
-                f"\033[31m[STDERR] Error opening file '{sys.argv[1]}': {e}\033[0m"
-            )
+        finally:
+            if file_open:
+                file_open.close()
+            if file_to_save:
+                file_to_save.close()
     else:
         print("\033[31mUsage: ft_ancient_text.py <file>\033[0m")
---
+++ ex3/ft_vault_security.py
@@ -0,0 +1,46 @@
+def secure_archive(
+    file_name: str, mode: int = 1, text_to_write: str = "Message"
+) -> tuple[bool, str]:
+    if mode == 1:
+        try:
+            open(file_name)
+        except FileNotFoundError as e:
+            return (False, str(e))
+        except PermissionError as e:
+            return (False, str(e))
+        content: str = ""
+        with open(file_name) as file:
+            content = file.read()
+        return (True, content)
+    elif mode == 2:
+        try:
+            open(file_name, "w")
+        except PermissionError as e:
+            return (False, str(e))
+        with open(file_name, "w") as file:
+            file.write(text_to_write)
+        return (True, "Content successfully written to file")
+    else:
+        return (False, f"{mode} is not correct mode")
+
+
+if __name__ == "__main__":
+    print("=== Cyber Archives Security ===")
+    print()
+    print("Using 'secure_archive' to read from a nonexistent file:")
+    file_state: tuple[bool, str] = secure_archive("/not/existing/file")
+    print(file_state)
+    print()
+    print("Using 'secure_archive' to read from an inaccessible file:")
+    file_state: tuple[bool, str] = secure_archive("/etc/master.passwd")
+    print(file_state)
+    print()
+    print("Using 'secure_archive' to read from a regular file:")
+    file_state: tuple[bool, str] = secure_archive("test.txt")
+    print(file_state)
+    print()
+    print("Using 'secure_archive' to write previous content to a new file:")
+    file_state: tuple[bool, str] = secure_archive(
+        "test.txt", 0, "affiche moi ca stp"
+    )
+    print(file_state)
